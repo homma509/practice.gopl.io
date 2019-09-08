7606f66a8d2e87f44589ad0fc19d304acc3c18dd
kokihonma <kouki.honma@gmail.com>
2019-09-09 05:09:35 +0900

    Mod: サブベンチマークを利用する形式に変更

--- ch01/ex03/main_test.go
+++ ch01/ex03/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"testing"
@@ -21,12 +22,24 @@ func bench(b *testing.B, n int, f func(io.Writer, ...string)) {
 	}
 }
 
-func BenchmarkEchoCat3(b *testing.B)      { bench(b, 3, EchoCat) }
-func BenchmarkEchoBuf3(b *testing.B)      { bench(b, 3, EchoBuf) }
-func BenchmarkEchoJoin3(b *testing.B)     { bench(b, 3, EchoJoin) }
-func BenchmarkEchoCat100(b *testing.B)    { bench(b, 100, EchoCat) }
-func BenchmarkEchoBuf100(b *testing.B)    { bench(b, 100, EchoBuf) }
-func BenchmarkEchoJoin100(b *testing.B)   { bench(b, 100, EchoJoin) }
-func BenchmarkEchoCat10000(b *testing.B)  { bench(b, 10000, EchoCat) }
-func BenchmarkEchoBuf10000(b *testing.B)  { bench(b, 10000, EchoBuf) }
-func BenchmarkEchoJoin10000(b *testing.B) { bench(b, 10000, EchoJoin) }
+func BenchmarkConcatenate(b *testing.B) {
+	benchCases := []struct {
+		name string
+		n    int
+		f    func(io.Writer, ...string)
+	}{
+		{"Cat", 3, EchoCat},
+		{"Buf", 3, EchoBuf},
+		{"Join", 3, EchoJoin},
+		{"Cat", 100, EchoCat},
+		{"Buf", 100, EchoBuf},
+		{"Join", 100, EchoJoin},
+		{"Cat", 10000, EchoCat},
+		{"Buf", 10000, EchoBuf},
+		{"Join", 10000, EchoJoin},
+	}
+	for _, c := range benchCases {
+		b.Run(fmt.Sprintf("%s%d", c.name, c.n),
+			func(b *testing.B) { bench(b, c.n, c.f) })
+	}
+}
