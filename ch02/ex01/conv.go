9546f5829288c2cf09cf21d1ccae4891d35ba1c9
kokihonma <kouki.honma@gmail.com>
2019-09-16 20:51:24 +0900

    Add: 練習問題2.1を追加

--- ch02/ex01/conv.go
+++ ch02/ex01/conv.go
@@ -3,11 +3,17 @@ package tempconv
 // CToF は摂氏の温度を華氏へ変換します。
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
+// CToK は摂氏の温度を絶対温度へ変換します。
+func CToK(c Celsius) Kelvin { return Kelvin(k + 273.15) }
+
 // FToC は華氏の温度を摂氏へ変換します。
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
-// KToC は絶対温度を摂氏へ変換します。
-func KToC(k Kelvin) Celsius { return Celsius(k - 273/15) }
+// FToK は華氏の温度を絶対温度へ変換します。
+func FToK(f Fahrenheit) Kelvin { return CToK(FToC(f)) }
 
-// CToF は摂氏の温度を絶対温度へ変換します。
-func CToK(c Celsius) Kelvin { return Kelvin(k + 273.15) }
+// KToC は絶対温度を摂氏へ変換します。
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// KToF は絶対温度を華氏へ変換します。
+func KToF(k Kelvin) Fahrenheit { return CToF(KToC(k)) }
--- ch02/ex01/tempconv.go
+++ ch02/ex01/tempconv.go
@@ -1,4 +1,3 @@
-// tempconv パッケージは摂氏（Celsius）と華氏（Fahrenheit）の温度変換を行います。
 package tempconv
 
 import "fmt"
